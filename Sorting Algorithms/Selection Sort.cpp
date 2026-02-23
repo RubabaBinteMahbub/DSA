cd0a4d9dee64a37e5f7d608c0682cee00143976a
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-02-24 00:46:57 +0600

    Add Selection Sort implementation in C++
    Implement selection sort algorithm to sort an array in increasing order.

---
+++ Sorting Algorithms/Selection Sort.cpp
@@ -0,0 +1,28 @@
+// Bubble sort
+
+// Time Complexity : O(n^2)
+// Space Complexity : O(n)
+
+// Sorting in increasing order
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int a[]={3, 5, 1, 2, 4};
+     int n=5, i, j, min_index;
+
+     for(int i=0; i<n-1; i++){
+          min_index =i;
+          for(j=i+1; j<n; j++){
+               if(a[j]<a[min_index])
+                    min_index=j;
+          }
+          swap(a[i], a[min_index]);
+     }
+
+     for( i=0; i<n; i++){
+       cout<<a[i]<<" ";
+     }
+
+     return 0;
+}
