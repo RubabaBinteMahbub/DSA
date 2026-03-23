c8ef83b5019b59e3553da65ec7f1d1aa6ca3dacb
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-03-23 14:56:31 +0600

    Update and rename ImplementationUsingArray.cpp to Stack-Array.cpp

--- Data Structure/Stack/Stack-Array.cpp
+++ Data Structure/Stack/Stack-Array.cpp
@@ -1,20 +1,23 @@
+// Implementation of stack using vector (implementing stack from scratch using array)
+
 #include<iostream>
 using namespace std;
 #define MAX 5
 class Stack{
- //private :
    int arr[MAX];
+
    int top;
-   public :
+   
+public :
    Stack(){
     top=-1; //top হচ্ছে সর্বশেষ element এর index।
    }
 
-   void push(int x){
+   void push(int val){
    if(top==MAX-1)
        cout<<"Stack overflow\n";
    else
-       arr[++top]=x;
+       arr[++top]=val;
    }
 
    void pop(){
@@ -23,7 +26,7 @@ class Stack{
    else
       top--;
    }
-  
+
   int peek(){
   if(top==-1) return -1;
   else return arr[top];
@@ -38,24 +41,24 @@ class Stack{
     cout<<"Stack is empty"<<endl;
     return;
     } //if(top==-1)
-   
+
   for(int i=top;i>=0;i--){
    //LIFO order এ প্রিন্ট করতে। যেটা stack এর natur।for(i=0;i<=top;i--) insertion order এ প্রিন্ট করতে। <= বা >=top না দিলে top element print হবে না। কারণ এটা top element এর index বোঝায়।
   cout<<arr[i]<<" ";
   }
   cout<<endl;
   }
- 
+
 };
 int main(){
-Stack st;
-st.push(10);
-st.push(20);
-st.push(30);
-  
-if(!st.isEmpty()){
-  st.printStack();
-}
-  
-return 0;
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    if(!s.isEmpty()){
+    s.printStack();
+    }
+    
+    return 0;
 }
