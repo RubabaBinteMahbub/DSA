f4a91aa7a2853f66c746320240fa48b15a4467cc
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-16 12:34:35 +0600

    Add insert function and improve printLL method

--- Data Structure/Linked List/code.cpp
+++ Data Structure/Linked List/code.cpp
@@ -1,7 +1,10 @@
+// Linked list from scrach
+
 /*
 Functions of linked list :
 push_front()    pop_front()
 push_back()    pop_back()
+insert()
 */
 
 #include<bits/stdc++.h>
@@ -23,28 +26,18 @@ class List{
     Node* tail;
 public :
     List(){
-    head = tail = NULL;   // as at first we have an empty list
+    head = tail = NULL;          // as at first we have an empty list
     }
 
     void push_front(int val){
         Node* newNode = new Node(val);
 
         if(head==NULL){
-           head=tail=newNode;   // case : it's an empty linked list
+           head=tail=newNode;     // case : it's an empty linked list
         }else{
-          newNode->next = head;// newNode এর next হিসেবে আগে NULL ছিল এখন বর্তমান head কে রাখলাম, আর newNode কে head হিসেবে। newNode কেন্দ্রিক চিন্তা-ভাবনা।
+          newNode->next = head;   // newNode এর next হিসেবে আগে NULL ছিল এখন বর্তমান head কে রাখলাম, আর newNode কে head হিসেবে। newNode কেন্দ্রিক চিন্তা-ভাবনা।
           head = newNode;
        }
-    }
-
-    void printLL(){
-       Node* temp = head;
-
-       while(temp != NULL){
-          cout<<temp->data<<" ";
-          temp = temp->next;
-       }
-       cout<<endl;
     }
 
     void push_back(int val){
@@ -59,54 +52,89 @@ public :
     }
 
     void pop_front(){
-         if(head == NULL){     // case : it's an empty linked list
+         if(head == NULL){           // case : it's an empty linked list
             cout<<"ll is empty"<<endl;
          }
 
-         Node* temp = head;   // head কে temp এ store করলাম
-         head = head->next;   // head এর next যে ছিল তাকে হেড এ রাখলাম
-         temp->next = NULL;   // temp (ex-head) এর next কে  NULL করে দেয়া তে এটা লিস্ট থেকে আলাদা হয়ে গেল
-         delete temp;         // temp (ex-head) কে delete করে দিলাম
+         Node* temp = head;         // head কে temp এ store করলাম
+         head = head->next;         // head এর next যে ছিল তাকে হেড এ রাখলাম
+         temp->next = NULL;         // temp (ex-head) এর next কে  NULL করে দেয়া তে এটা লিস্ট থেকে আলাদা হয়ে গেল
+         delete temp;               // temp (ex-head) কে delete করে দিলাম
     }
 
     void pop_back(){
-        if(head == NULL){    // case : it's an empty linked list
+        if(head == NULL){          // case : it's an empty linked list
             cout<<"ll is empty"<<endl;
         }
 
         Node* temp = head;
-        while(temp->next != tail){  // to find the second last element or previous element as we want to erase the "next (linking pointer / connector -next erase মানে এটাকে  NULL করে দেওয়া)" of that element
-            temp = temp->next;      // traversing to the second last element
+        while(temp->next != tail){ // to find the second last element or previous element as we want to erase the "next (linking pointer / connector -next erase মানে এটাকে  NULL করে দেওয়া)" of that element
+            temp = temp->next;     // traversing to the second last element
         }
         temp->next = NULL;         // second last element এর next কে NULL করে দিলাম , তাই লাস্ট element লিস্ট থেকে আলাদা হয়ে গেল
         delete tail;               // deletes the data of tail variable, not the variable itself
         tail = temp;
     }
+
+     void insert( int val, int pos){
+        if(pos < 0){
+            cout<<"Invalid position"<<endl;
+            return;
+        }
+
+        if(pos == 0){
+            push_front(val);
+            return;
+        }
+
+        Node* newNode = new Node(val);
+        Node* temp = head;
+        for(int i=0; i<pos; i++){
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    void printLL(){
+       Node* temp = head;
+
+       while(temp != NULL){
+          cout<<temp->data<<" ";
+          temp = temp->next;
+       }
+       cout<<endl;
+    }
 };
 
 int main()
 {
-   List ll;
+    List ll;
 
-   ll.push_front(1);
-   ll.push_front(2);
-   ll.push_front(3);
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_front(4);
 
-   ll.printLL();
+    ll.printLL();
 
-   ll.push_back(4);
+    ll.push_back(5);
 
-   ll.printLL();
+    ll.printLL();
 
-   ll.pop_front();
+    ll.pop_front();
 
-   ll.printLL();
+    ll.printLL();
 
-   ll.pop_back();
+    ll.pop_back();
 
-   ll.printLL();
+    ll.printLL();
 
-   return 0;
+    ll.insert(6,1);
+
+    ll.printLL();
+
+    return 0;
 }
 
 // Task : Visualise the proceses that how we are thinking to solve the problem
