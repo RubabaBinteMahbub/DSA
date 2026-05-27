233594d0e420162fdbd4ea81f2978d67370aae1c
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-27 12:04:26 +0600

    Update pangram.cpp

--- Map/pangram.cpp
+++ Map/pangram.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void solve(){
+    int n;
+    cin>>n;
+    
+    string s;
+    cin>>s;
+    
+    map<char,int>mp;
+    for(int i=0; i<n; i++){
+        if(s[i]>='A'&& s[i]<='Z'){
+            s[i]+=32;
+        }
+        mp[s[i]]++;
+    }
+    
+    //int cnt=mp.size();
+    int cnt=0;
+    for(auto x : mp){
+      if(x.second>=1) cnt++;
+    }
+    
+    if(cnt==26) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
