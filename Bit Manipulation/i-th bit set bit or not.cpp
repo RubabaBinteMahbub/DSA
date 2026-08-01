c464bbce16bbfcefa39918cbc3b1a9e12146a3ba
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-08-02 00:08:01 +0600

    Implement i-th bit check using LS and RS methods
    Added functions to check if the i-th bit of a number is set using left and right shift methods.

--- Bit Manipulation/i-th bit set bit or not.cpp
+++ Bit Manipulation/i-th bit set bit or not.cpp
@@ -6,3 +6,35 @@ Right shift :
 ১) সংখ্যাটাকে i বার right shift করতে হবে। (n=)।
 ২) 1 এর সাথে & করতে হবে।
 ৩) 0 হলে সেট(1) না। non-zero হলে সেট (1)।*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// i-th bit set or not
+bool checkIthBit_LS(int n, int i){
+     return ((1<<i)&n)!=0;
+}
+
+bool checkIthBit_RS(int n, int i){
+    return ((n>>i)&1) != 0;
+}
+
+int main()
+{
+int num = 5;
+int bitIndex = 2;
+
+if(checkIthBit_LS(num, bitIndex)){
+    cout<<"Yes (LS)"<<endl;
+}else{
+    cout<<"No (LS)"<<endl;
+}
+
+if(checkIthBit_RS(num, bitIndex)){
+    cout<<"Yes (RS)"<<endl;
+}else{
+    cout<<"No (RS)"<<endl;
+}
+
+return 0;
+}
